66a37ec321cf82e6cf413c86c7aeeb160e2d9ce2
geigea84 <geigea84@gmail.com>
2020-11-17 22:35:47 -0600

    finished 3.3

--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,17 @@ CREATE TABLE candidates (
     last_name VARCHAR(30) NOT NULL,
     industry_connected BOOLEAN NOT NULL
 );
+
+CREATE TABLE parties (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    description TEXT
+);
+
+/* 3.3
+With the parties table added to the schema, run the following migrate script again:
+
+npm run migrate
+
+Remember, npm run migrate was a custom script we added to the package.json file to run sqlite3 db/election.db < db/schema.sql for us.
+ */
